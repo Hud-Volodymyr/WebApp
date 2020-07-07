cc2d7225e3598e1944099eb777c16d503d851944
Hud-Volodymyr <v.gud2001@gmail.com>
2020-07-07 23:13:50 +0300

    Update submit

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,10 +1,13 @@
 'use strict';
+const models = require('../models');
 // eslint-disable-next-line no-unused-vars
 exports.getLanding = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 };
 // eslint-disable-next-line no-unused-vars
 exports.submitLead = function(req, res, next) {
-  console.log('lead_email:', req.body.lead_email);
-  res.redirect('/');
+  return models.Lead.create({
+    email: req.body.lead_email,
+  // eslint-disable-next-line no-unused-vars
+  }).then(lead => res.redirect('/'));
 };
