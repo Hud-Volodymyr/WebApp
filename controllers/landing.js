1f2a538da1ba25e1c5dea3e67d8e6a6b90766481
Hud-Volodymyr <v.gud2001@gmail.com>
2020-07-14 21:02:17 +0300

    Add lead edit and post routes

--- controllers/landing.js
+++ controllers/landing.js
@@ -5,7 +5,7 @@ exports.getLanding = (req, res, next) =>
   res.render('landing', { title: 'Express' });
 
 exports.submitLead = (req, res, next) => models.Lead.create({
-  email: req.body.lead_email,
+  email: req.body.leadEmail,
 }).then(lead => res.redirect('/leads'));
 
 exports.showLeads = (req, res, next) =>
@@ -24,3 +24,24 @@ exports.showLead = (req, res, next) =>
   }).then(lead => {
     res.render('lead', { lead });
   });
+
+exports.showEditLead = (req, res, next) =>
+  models.Lead.findOne({
+    where: {
+      id: req.params.leadId
+    }
+  }).then(lead => {
+    res.render('lead/editLead', { lead });
+  });
+
+exports.editLead = (req, res, next) =>
+  models.Lead.update({
+    email: req.body.leadEmail
+  }, {
+    where: {
+      id: req.params.leadId
+    }
+  }).then(result => {
+    res.redirect('/lead/' + req.params.leadId);
+  });
+
